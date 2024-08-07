30b46270b21d7d8d260e91629a1b6352930cd1fc
ananyatewari <ananyatewari0205@gmail.com>
2024-08-07 09:34:19 +0530

    made array of objects in main.cpp

---
+++ src/User.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class User {
+private:
+    string username;
+    string password;
+
+public:
+    User(string username, string password) {
+        this->username = username;
+        this->password = password;
+    }
+
+    void setPassword(string p) {
+        password = p;
+    }
+
+    string getUsername() {
+        return username;
+    }
+
+    string getPassword() {
+        return password;
+    }
+
+    void displayUserDetails() {
+        cout << "Username: " << username << endl;
+    }
+};
