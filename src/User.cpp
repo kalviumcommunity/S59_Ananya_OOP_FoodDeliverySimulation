244c76eb4fc93420984271672e64eb28d95720c6
ananyatewari <ananyatewari0205@gmail.com>
2024-08-13 14:39:29 +0530

    added static variables in the project

--- src/User.cpp
+++ src/User.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 class User {
-private:
+protected:
     string username;
     string password;
+    string role;
 
 public:
-    User(string username, string password) {
-        this->username = username;
-        this->password = password;
+    User(string username, string password, string role) {
+        this -> username = username;
+        this -> password = password;
+        this -> role = role;
     }
 
     void setPassword(string p) {
@@ -27,4 +30,40 @@ public:
     void displayUserDetails() {
         cout << "Username: " << username << endl;
     }
+
+    void login() {
+        string user, pass;
+        cout << "Enter username to login: ";
+        cin >> user;
+        cout << "Enter password for verification: ";
+        cin >> pass;
+
+        if (user == getUsername() && pass == getPassword()) {
+            cout << "Login successful! Logged in as " << role << endl;
+        } 
+        else {
+            cout << "Login failed! Incorrect credential(s)" << endl;
+            exit(0);
+        }
+    }
+};
+
+class Customer : public User {
+private:
+    static int orderCounter;
+    static int numberOfOrders;
+public:
+    Customer(string username, string password) : User(username, password, "Customer") {}
+};
+
+int Customer :: orderCounter = 0;
+int Customer :: numberOfOrders = 0;
+
+class RestaurantOwner : public User {
+private:
+    int secretCode;
+    string restaurantName;
+
+public:
+    RestaurantOwner(string username, string password, string restaurant, int secret) : User(username, password, "RestaurantOwner"), restaurantName(restaurant), secretCode(secret) {}
 };
